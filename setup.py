8efef45452bee1013f24d87d6bb47abf179cfe73
antoinelnrld <38052726+antoinelrnld@users.noreply.github.com>
2023-07-18 16:11:35 +0200

    fix: filled setup.py fields to allow package publication

--- setup.py
+++ setup.py
@@ -12,6 +12,16 @@ setuptools.setup(
     package_data={"splitter": ["*"]},
     install_requires=requirements,
     author="AXA",
+    author_email="guillaume.chervet@axa.fr",
+    url="https://github.com/AxaFrance/axa-fr-splitter",
+    description="AXA France file splitter package",
+    long_description="Utility library used to split pages from common file "
+                     "types (pdf, tiff) into separate png files. It also "
+                     "extracts text from input files.",
+    classifiers=["Programming Language :: Python :: 3 :: Only",
+                 "Programming Language :: Python :: 3.8",
+                 "Topic :: Scientific/Engineering :: Information Analysis",
+                 ],
     include_package_data=True,
     python_requires=">=3.8",
 )
